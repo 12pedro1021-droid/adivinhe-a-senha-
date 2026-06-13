292e84d07f9654cdcde2d098c487beab926d2bd7
Pedro Henrique <12pedro1021@gmail.com>
2026-06-13 13:15:38 -0300

    Ampliar testes do jogo

--- test_main.py
+++ test_main.py
@@ -5,6 +5,12 @@ import main
 
 
 class TestAdivinhaSenha(unittest.TestCase):
+    def test_gerar_senha_fica_no_intervalo_configurado(self):
+        for _ in range(100):
+            senha = main.gerar_senha()
+            self.assertGreaterEqual(senha, main.MENOR_SENHA)
+            self.assertLessEqual(senha, main.MAIOR_SENHA)
+
     def test_avaliar_palpite_quando_acerta(self):
         self.assertEqual(main.avaliar_palpite(42, 42), "acertou")
 
@@ -13,6 +19,12 @@ class TestAdivinhaSenha(unittest.TestCase):
 
     def test_avaliar_palpite_quando_palpite_e_baixo(self):
         self.assertEqual(main.avaliar_palpite(10, 42), "baixo")
+
+    def test_mostrar_dica_exibe_mensagem_de_acerto(self):
+        with patch("builtins.print") as print_mock:
+            main.mostrar_dica("acertou")
+
+        print_mock.assert_called_once_with("Acertou! A porta foi desbloqueada.")
 
     def test_jogar_ignora_entradas_invalidas_sem_gastar_tentativa(self):
         entradas = iter(["abc", "0", "42"])
